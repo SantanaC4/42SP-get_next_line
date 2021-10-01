a98eacbd12358dae36c113658430dd165dc9b782
SantanaC4 <eduardo.sanati@gmail.com>
2021-09-30 21:02:26 -0300

    Update

--- get_next_line.h
+++ get_next_line.h
@@ -1,43 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: edrodrig <edrodrig@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/09/19 22:46:26 by edrodrig          #+#    #+#             */
-/*   Updated: 2021/09/30 20:57:30 by edrodrig         ###   ########.fr       */
+/*   Created: 2021/09/30 20:12:44 by edrodrig          #+#    #+#             */
+/*   Updated: 2021/09/30 20:57:04 by edrodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "get_next_line.h"
-
-char	*get_next_line(int fd)
-{
-	char *buff;
-	char val;
-	unsigned char i;
-
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
-
-	buff = malloc(sizeof(char) * (BUFFER_SIZE + 1));	
-	if (buff == NULL)
-		return (NULL);
-
-	i = 0;
-	while (read(fd, &val, 1) > 0)
-	{
-		if (val != '\n')
-		{
-			buff[i] = val;
-			i++;
-		}
-		else
-		{
-			buff[i + 1] = '\0';
-			break;
-		}
-	}
-	return (buff);
-}
+#ifndef GET_NEXT_LINE_H
+#define GET_NEXT_LINE_H
+#include <stddef.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <stdio.h> 
+char	*get_next_line(int fd);
+#endif
