82d857c84114c4a7c461afeb75aef5ce0cd7b79b
SantanaC4 <eduardo.sanati@gmail.com>
2021-10-16 00:14:56 -0300

    Update

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: edrodrig <edrodrig@student.42sp.org.b      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/30 20:12:44 by edrodrig          #+#    #+#             */
-/*   Updated: 2021/10/15 23:52:27 by edrodrig         ###   ########.fr       */
+/*   Updated: 2021/10/16 00:11:26 by edrodrig         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 # include <stddef.h>
 # include <stdlib.h>
 # include <unistd.h>
